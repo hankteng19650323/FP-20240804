nessied/mazda3_2018

Vehicles - Subaru - Subaru Crosstrek Torque Increase
Increases the maximum allowed torque for the Subaru Crosstrek.

selfdrive/car/subaru/values.py
@@ -26,6 +26,8 @@ class CarControllerParams:
       self.STEER_DELTA_DOWN = 40
     elif CP.carFingerprint == CAR.IMPREZA_2020:
       self.STEER_MAX = 1439
+    elif CP.carFingerprint == CAR.IMPREZA and Params().get_bool("CrosstrekTorque"):
+      self.STEER_MAX = 3071
     else:
       self.STEER_MAX = 2047
 

hankteng19650323/FP-20240804

selfdrive/car/subaru/values.py
@@ -26,6 +26,8 @@ class CarControllerParams:
       self.STEER_DELTA_DOWN = 40
     elif CP.carFingerprint == CAR.SUBARU_IMPREZA_2020:
       self.STEER_MAX = 1439
+    elif CP.carFingerprint == CAR.SUBARU_IMPREZA and Params().get_bool("CrosstrekTorque"):
+      self.STEER_MAX = 3071
     else:
       self.STEER_MAX = 2047
 